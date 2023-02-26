c56bb835d62d07c8b24355d46265ce7f0c0f1e45
ananta <12180395+ananta@users.noreply.github.com>
2023-02-26 12:08:07 -0500

    feat(server): setup a basic authentication server with actix-web

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,86 +1,90 @@
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate rocket_sync_db_pools;
+use actix_web::{
+    dev::ServiceRequest,
+    error::Error,
+    web::{self, Data},
+    App, HttpMessage, HttpServer,
+};
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
+use actix_web_httpauth::{
+    extractors::{
+        bearer::{self, BearerAuth},
+        AuthenticationError,
+    },
+    middleware::HttpAuthentication,
+};
+use hmac::{Hmac, Mac};
+use jwt::VerifyWithKey;
+use serde::{Deserialize, Serialize};
+use sha2::Sha256;
 
-mod auth;
-mod models;
-mod schema;
-
-use auth::BasicAuth;
-use diesel::QueryDsl;
-use models::Rustacean;
-use rocket::response::*;
-use rocket::response::status::*;
-use rocket::serde::json::*;
-use rocket_sync_db_pools::diesel::SqliteConnection;
-use schema::rustaceans;
-use crate::diesel::RunQueryDsl;
-
-
-#[database("sqlite_logs")]
-struct LogsDbConn(SqliteConnection);
-
-#[database("sqlite_path")]
-struct DbConn(SqliteConnection);
-
-
-#[get("/rustaceans")]
-async fn get_rustaceans(_auth: BasicAuth, _conn: DbConn) -> Value {
-    // use schema::rustaceans::dsl::*;
-    _conn.run(|c| {
-        let all = rustaceans::table.limit(100).load::<Rustacean>(c).expect("error loading rustaceans from db");
-        json!(all)
-    }).await
+pub struct AppState {
+    db: Pool<Postgres>
 }
 
-#[get("/decks")]
-fn get_decks(_auth: BasicAuth) -> Value {
-    json!([
-        { "id": 1, "name": "First Deck"},
-        { "id": 2, "name": "Second Deck"},
-        { "id": 3, "name": "Third Deck"},
-        { "id": 4, "name": "Fourth Deck"},
-    ])
+// bring them in
+mod services;
+use services::{basic_auth, create_user, create_deck};
+
+// define structure of our bearer token
+// should be serializeable, deserialzable and cloneable
+#[derive(Serialize, Deserialize, Clone)]
+pub struct TokenClaims {
+    id: i32,
 }
 
-#[get("/decks/<id>")]
-fn view_deck(id: i32, _auth: BasicAuth) -> Value {
-    json!({ "id": id, "name": "First Deck"})
+// middleware to validate our token
+async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    let jwt_secret: String = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set!");
+    let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_secret.as_bytes()).unwrap();
+    let token_string = credentials.token();
+
+    // verify the token,
+    let claims: Result<TokenClaims, &str> = token_string.verify_with_key(&key)
+        .map_err(|_| "Invalid token");
+    match claims {
+        // get the value if exists
+        // and add the data into our request
+        Ok(value) => {
+            req.extensions_mut().insert(value);
+            Ok(req)
+        }
+        Err(_) => {
+            let config = req.app_data::<bearer::Config>()
+                .cloned()
+                .unwrap_or_default()
+                .scope("");
+            Err((AuthenticationError::from(config).into(), req))
+        }
+    }
+
 }
 
-#[post("/decks", format = "json")]
-fn create_deck(_auth: BasicAuth) -> Value {
-    json!({ "id": 99, "name": "New Deck"})
-}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Error building a connection pool");
 
-#[put("/decks/<id>", format = "json")]
-fn update_deck(id: i32, _auth: BasicAuth) -> Value {
-    json!({ "id": id, "name": "Updated Deck"})
-}
 
-#[delete("/decks/<_id>", format = "json")]
-fn delete_deck(_id: i32, _auth: BasicAuth) -> NoContent {
-    // delete something in the database
-    status::NoContent
+    HttpServer::new(move || {
+        let bearer_middleware = HttpAuthentication::bearer(validator);
+        App::new()
+            .app_data(Data::new(AppState { db: pool.clone() }))
+            .service(basic_auth)
+            .service(create_user)
+            .service(
+                web::scope("")
+                .wrap(bearer_middleware)
+                    .service(create_deck),
+            )
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+    .await
 }
-
-#[catch(404)]
-fn not_found() -> Value {
-    json!("Not found!")
-}
-
-#[rocket::main]
-async fn main() {
-    let _ = rocket::build().mount("/", routes![
-        get_decks,
-        view_deck,
-        create_deck,
-        update_deck,
-        delete_deck
-    ])
-    .register("/",catchers![not_found])
-    .attach(DbConn::fairing())
-    .launch().await;
-}
-
