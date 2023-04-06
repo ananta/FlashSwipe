5acb08a58f35336b131ec8b265ebd0ae9c4f710c
ananta <12180395+ananta@users.noreply.github.com>
2023-04-06 12:10:43 -0400

    feat(zustand): migrate to zustand

--- app/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "../slices/authSlice";
-
-export const store = configureStore({
-  reducer: {
-    userAuth: authSlice
-  }
-});
