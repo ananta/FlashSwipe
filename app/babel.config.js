29970ac21990708fe73ab2858353a44051b897f5
ananta <12180395+ananta@users.noreply.github.com>
2023-04-07 23:06:27 -0400

    chore(configs): prettier & module-resolver config

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,6 +1,32 @@
-module.exports = function(api) {
-  api.cache(true);
+process.env.TAMAGUI_TARGET = 'native'
+
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json', '.svg', '.jpg'],
+        },
+      ],
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+        },
+      ],
+      [
+        'transform-inline-environment-variables',
+        {
+          include: 'TAMAGUI_TARGET',
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  }
+}
