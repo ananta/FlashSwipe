4aca3b5d7084b3a91970c7d04953ae8ba02db525
ananta <12180395+ananta@users.noreply.github.com>
2023-04-09 21:38:33 -0400

    feat(app:auth): add auth screens

--- app/src/screens/auth/LoginScreen.tsx
+++ app/src/screens/auth/LoginScreen.tsx
@@ -1,60 +1,113 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { useDispatch } from "react-redux";
-import { setSignIn } from "../../slices/authSlice";
+import React, { Fragment } from 'react'
+import { Input, YStack, Label, Text, SizableText } from 'tamagui'
+import { useForm, Controller, SubmitHandler } from 'react-hook-form'
 
-const LoginScreen = () => {
-  const dispatch = useDispatch();
+import { useStore } from 'store'
+import { Button } from 'components/Button'
+import { LogoItem } from 'components/Logo'
+import { Layout } from 'components/Layout'
+import { NavProps } from 'types/NavTypes'
+import { IUserCredentials } from 'store/authSlice'
 
-  const handleLogin = () => {
-    const user = {
-      isLoggedIn: true,
-      email: "jdoe@test.com",
-      userName: "johnDoe"
-    };
+const LoginScreen: React.FC<NavProps> = ({ navigation }) => {
+  const { login, isLoading } = useStore((state) => state)
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setFocus,
+  } = useForm<IUserCredentials>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  })
 
-    dispatch(setSignIn(user));
-  };
+  const handleLogin: SubmitHandler<IUserCredentials> = (credentials) =>
+    login(credentials)
 
   return (
-    <View style={styles.container}>
-      <Text style={{ marginBottom: 20, fontSize: 15 }}>Flash Swipe</Text>
-      <TouchableOpacity onPress={handleLogin} style={styles.btn}>
-        <Text style={styles.text}>Sign In</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={handleLogin}
-        style={[
-          styles.btn,
-          {
-            backgroundColor: "red",
-            marginTop: 10
-          }
-        ]}
-      >
-        <Text style={styles.text}>Register</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+    <Layout>
+      <YStack p='$3' space='$2'>
+        <LogoItem />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <Fragment>
+              <Label>Username</Label>
+              <Input
+                ref={ref}
+                borderWidth={2}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                disabled={isLoading}
+                value={value}
+                autoCapitalize='none'
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onSubmitEditing={() => setFocus('password')}
+              />
+              {errors.username && (
+                <SizableText size='$3' theme='alt1' color='red'>
+                  This is required
+                </SizableText>
+              )}
+            </Fragment>
+          )}
+          name='username'
+        />
 
-export default LoginScreen;
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <Fragment>
+              <Label>Password</Label>
+              <Input
+                ref={ref}
+                borderWidth={2}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                disabled={isLoading}
+                value={value}
+                autoCapitalize='none'
+                returnKeyType='done'
+                onSubmitEditing={handleSubmit(handleLogin)}
+              />
+              {errors.password && (
+                <SizableText size='$3' theme='alt1' color='red'>
+                  This is required
+                </SizableText>
+              )}
+            </Fragment>
+          )}
+          name='password'
+        />
+        <Label>
+          Forgot Password ? {` `}
+          <Text color={'skyblue'} onPress={() => navigation.navigate('Login')}>
+            Reset Here
+          </Text>
+        </Label>
+        <Button
+          theme='purple'
+          mt='$2'
+          onPress={handleSubmit(handleLogin)}
+          isLoading={isLoading}
+        >
+          Sign in
+        </Button>
+        <Button theme='alt2' onPress={() => navigation.navigate('Register')}>
+          Register
+        </Button>
+      </YStack>
+    </Layout>
+  )
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  btn: {
-    backgroundColor: "blue",
-    paddingHorizontal: 50,
-    paddingVertical: 10,
-    borderRadius: 10
-  },
-  text: {
-    color: "white",
-    fontSize: 20
-  }
-});
+export default LoginScreen
--- app/src/screens/auth/RegisterScreen.tsx
+++ app/src/screens/auth/RegisterScreen.tsx
@@ -1,19 +1,178 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect } from 'react'
+import { Input, YStack, Label, Text, SizableText } from 'tamagui'
+import { useForm, SubmitHandler, Controller } from 'react-hook-form'
 
-const RegisterScreen = () => {
+import { LogoItem } from 'components/Logo'
+import { Layout } from 'components/Layout'
+import { NavProps } from 'types/NavTypes'
+import { Button } from 'components/Button'
+import { useStore } from 'store'
+import { IUserCredentials, IUserProfile } from 'store/authSlice'
+
+const RegisterScreen: React.FC<NavProps> = ({ navigation }) => {
+  const { register, isLoading, isSuccess } = useStore((state) => state)
+  const {
+    control,
+    handleSubmit,
+    reset: resetForm,
+    formState: { errors },
+    setFocus,
+  } = useForm<IUserProfile & IUserCredentials>({
+    defaultValues: {
+      first_name: '',
+      last_name: '',
+      username: '',
+      password: '',
+    },
+  })
+
+  const handleRegister: SubmitHandler<IUserCredentials & IUserProfile> = (
+    newUser
+  ) => register(newUser)
+
+  useEffect(() => {
+    if (!isLoading && isSuccess) {
+      resetForm()
+    }
+  }, [isSuccess, isLoading])
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF"
-      }}
-    >
-      <Text style={{ marginBottom: 20, fontSize: 15 }}>Login Screen</Text>
-    </View>
-  );
-};
+    <Layout>
+      <YStack p='$3' space='$2'>
+        <LogoItem />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <>
+              <Label>First Name</Label>
+              <Input
+                borderWidth={2}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                ref={ref}
+                autoCapitalize='none'
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onSubmitEditing={() => setFocus('last_name')}
+              />
+              {errors.first_name && (
+                <SizableText size='$3' theme='alt1' color='red'>
+                  This is required
+                </SizableText>
+              )}
+            </>
+          )}
+          name='first_name'
+        />
 
-export default RegisterScreen;
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <>
+              <Label>Last Name</Label>
+              <Input
+                ref={ref}
+                borderWidth={2}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                autoCapitalize='none'
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onSubmitEditing={() => setFocus('username')}
+              />
+              {errors.last_name && (
+                <SizableText size='$3' theme='alt1' color='red'>
+                  This is required
+                </SizableText>
+              )}
+            </>
+          )}
+          name='last_name'
+        />
+
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <>
+              <Label>Username</Label>
+              <Input
+                ref={ref}
+                borderWidth={2}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                autoCapitalize='none'
+                returnKeyType='next'
+                returnKeyLabel='next'
+                onSubmitEditing={() => setFocus('password')}
+              />
+              {errors.username && (
+                <SizableText size='$3' theme='alt1' color='red'>
+                  This is required
+                </SizableText>
+              )}
+            </>
+          )}
+          name='username'
+        />
+
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <>
+              <Label>Password</Label>
+              <Input
+                ref={ref}
+                borderWidth={2}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                autoCapitalize='none'
+                returnKeyType='done'
+                returnKeyLabel='done'
+                onSubmitEditing={handleSubmit(handleRegister)}
+              />
+              {errors.password && (
+                <SizableText size='$3' theme='alt1' color='red'>
+                  This is required
+                </SizableText>
+              )}
+            </>
+          )}
+          name='password'
+        />
+        <Button
+          theme='purple'
+          mt='$2'
+          onPress={handleSubmit(handleRegister)}
+          isLoading={isLoading}
+        >
+          Register
+        </Button>
+
+        <Label>
+          Already have an Account?{' '}
+          <Text color={'skyblue'} onPress={() => navigation.navigate('Login')}>
+            Login here
+          </Text>
+        </Label>
+      </YStack>
+    </Layout>
+  )
+}
+
+export default RegisterScreen
